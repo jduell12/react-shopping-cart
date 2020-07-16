3b3ae3c924d52f9baa28f7675c407bf5bc297255
Jessica <duellje@oregonstate.edu>
2020-07-16 14:25:59 -0400

    added context to navigation and cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 //Context
 import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,14 +22,16 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+				<CartContext.Provider value={{cart}}>
+					<Navigation />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
